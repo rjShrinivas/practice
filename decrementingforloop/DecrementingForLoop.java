11d6248bd449778dc7e050156786595a526eb431
ShrinivasJeevapur <shrinivasjeevapur@gmail.com>
2023-08-17 23:56:11 +0530

    Text Modified

--- decrementingforloop/DecrementingForLoop.java
+++ decrementingforloop/DecrementingForLoop.java
@@ -8,7 +8,7 @@ public class DecrementingForLoop {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the range");
 		int num = scan.nextInt();
-		for (int i = num; i >= 1; i--) {
+		for (int i = num; i >= 2; i--) {
 			System.out.println(i);
 			scan.close();
 		}
